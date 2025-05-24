292c8d3f4fdfb44f54a400856419d6e18ca20a1c
Guilherme <guilherme.apnascimento@hotmail.com>
2025-05-23 22:33:04 -0300

    %atualizado para dados de 2025

--- app/src/main/java/com/example/icms/MainActivity.kt
+++ app/src/main/java/com/example/icms/MainActivity.kt
@@ -47,11 +47,33 @@ class MainActivity : AppCompatActivity() {
 
     fun obterICMS(sigla: String): Double {
         return when (sigla) {
-            "SP" -> 18.0
-            "RJ" -> 20.0
-            "MG" -> 18.0
+            "AC" -> 19.0
+            "AL" -> 20.0
+            "AP" -> 18.0
+            "AM" -> 20.0
+            "BA" -> 20.5
+            "CE" -> 20.0
+            "DF" -> 20.0
             "ES" -> 17.0
-            "BA" -> 18.0
+            "GO" -> 19.0
+            "MA" -> 23.0
+            "MT" -> 17.0
+            "MS" -> 17.0
+            "MG" -> 18.0
+            "PA" -> 19.0
+            "PB" -> 20.0
+            "PR" -> 19.5
+            "PE" -> 20.5
+            "PI" -> 22.5
+            "RJ" -> 22.0
+            "RN" -> 20.0
+            "RS" -> 17.0
+            "RO" -> 19.5
+            "RR" -> 20.0
+            "SC" -> 17.0
+            "SP" -> 18.0
+            "SE" -> 20.0
+            "TO" -> 20.0
             else -> 18.0
         }
     }
